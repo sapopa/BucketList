c8167b2ca54b19b70b18c8c6a56c932f848b170f
sebastian.popa <sebastian.popa@softvision.com>
2024-01-10 10:05:15 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by sebastian.popa on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by sebastian.popa on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
